db66419f41d79ec468150c416a7929e5e0c9c4ee
Romain Tartière <romain@blogreen.org>
2024-04-08 11:23:11 -1000

    bundle gem opensearch-ruby-cli

---
+++ lib/opensearch/ruby/cli.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "cli/version"
+
+module Opensearch
+  module Ruby
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/opensearch/ruby/cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Opensearch::Ruby::Cli do
+  it "has a version number" do
+    expect(Opensearch::Ruby::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
